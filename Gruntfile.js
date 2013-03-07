a11f411b1648172f9ac82db92928d6d0f9fc0280
Hunter Clarke <hunter@hunterc.com>
2013-03-06 21:14:06 -0500

    Added dev and watch tasks to the grunt file.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,21 +6,33 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-compass');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     // Project Configuration
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+
         coffee: {
-            compile: {
+            build: {
                 files: {
-                    './build/application.js': ['./src/models/*.coffee'
-                                                        , './src/routers/*.coffee'
-                                                        , './src/views/*.coffee']
+                    './build/app.js': ['./src/models/*.coffee'
+                                        , './src/routers/*.coffee'
+                                        , './src/views/*.coffee'
+                                        , './src/app.coffee']
+                }
+            },
+            dev: {
+                files: {
+                    './public/javascripts/app.js': ['./src/models/*.coffee'
+                                                    , './src/routers/*.coffee'
+                                                    , './src/views/*.coffee'
+                                                    , './src/app.coffee']
                 }
             }
         },
+
         handlebars: {
-            compile: {
+            build: {
                 options: {
                     // Put namespace in here
                     namespace: 'Project.Templates',
@@ -37,35 +49,89 @@ module.exports = function(grunt) {
                 files: {
                     './build/templates.js': './src/templates/*/*.hbs'
                 }
+            },
+            dev: {
+                options: {
+                    // Put namespace in here
+                    namespace: 'Project.Templates',
+                    processName: function(path) {
+                        var pieces = path.split('/');
+                        var idx = pieces.indexOf('templates');
+                        var path = '';
+                        for (var i=idx+1; i<pieces.length-1; i++)
+                            path = path+pieces[i]+'/';
+                        path = path + pieces[pieces.length-1].split('.')[0];
+                        return path;
+                    }
+                },
+                files: {
+                    './public/javascripts/templates.js': './src/templates/*/*.hbs'
+                }
             }
         },
+
         compass: {
-            dev :{
+            build: {
                 options: {
                     sassDir: './src/style',
                     cssDir: './public/stylesheets',
                     outputStyle: 'compressed'
                 }
-            }
-        },
-        uglify: {
-            my_target: {
-                files: {
-                    './public/javascripts/application.js': ['./src/lib/jquery-1.9.1.min.js'
-                                                        , './src/lib/json2.js'
-                                                        , './src/lib/underscore-min.js'
-                                                        , './src/lib/backbone-min.js'
-                                                        , './src/lib/handlebars.js'
-                                                        , './build/application.js'],
-                    './public/javascripts/templates.js': './build/templates.js'
+            },
+            dev :{
+                options: {
+                    sassDir: './src/style',
+                    cssDir: './public/stylesheets'
                 }
             }
         },
+
+        uglify: {
+            build: {
+                files: {
+                    './public/javascripts/lib.js': ['./src/lib/jquery-1.9.1.min.js'
+                                                    , './src/lib/json2.js'
+                                                    , './src/lib/underscore-min.js'
+                                                    , './src/lib/backbone-min.js'
+                                                    , './src/lib/handlebars.js'],
+                    './public/javascripts/templates.js': './build/templates.js',
+                    './public/javascripts/app.js': './build/app.js'
+                }
+            },
+            dev: {
+                './public/javascripts/lib.js': ['./src/lib/jquery-1.9.1.min.js'
+                                                , './src/lib/json2.js'
+                                                , './src/lib/underscore-min.js'
+                                                , './src/lib/backbone-min.js'
+                                                , './src/lib/handlebars.js']
+            }
+        },
+
         clean: {
             clean: ['./build']
+        },
+
+        watch: {
+            templates: {
+                files: ['./src/templates/*/*.hbs'],
+                tasks: ['handlebars:dev']
+            },
+            coffee: {
+                files: ['./src/models/*.coffee'
+                        , './src/routers/*.coffee'
+                        , './src/views/*.coffee'
+                        , './src/app.coffee'],
+                tasks: ['coffee:dev']
+            },
+            sass: {
+                files: './src/style/*.scss',
+                tasks: ['compass:dev']
+            }
         }
     });
 
-    // Register the default task
-    grunt.registerTask('default',['coffee', 'handlebars', 'uglify', 'clean']);
+    grunt.registerTask('build',['coffee:build', 'handlebars:build', 'uglify:build', 'compass:build', 'clean']);
+    grunt.registerTask('dev', ['coffee:dev', 'handlebars:dev', 'uglify:dev', 'compass:dev']);
+    grunt.registerTask('style', ['compass']);
+    grunt.registerTask('js', ['coffee', 'handlebars'])
 };
